a3c6bd782f38a4073543ade20bd539b9e6e0e7aa
csy86 <vcsy86v@gmail.com>
2026-05-11 17:15:16 +0900

    init commit

---
+++ app.js
@@ -0,0 +1,30 @@
+const img1 = document.querySelector(".img1");
+const img2 = document.querySelector(".img2");
+const title = document.querySelector(".container > h1");
+img1.src = "/Dicee Challenge/images/dice6.png";
+img2.src = "/Dicee Challenge/images/dice6.png";
+
+title.addEventListener("click", diceGame);
+
+function diceGame() {
+  let resultNum1 = setDice();
+  let resultNum2 = setDice();
+  changeDice(img1, resultNum1);
+  changeDice(img2, resultNum2);
+
+  if (resultNum1 > resultNum2) {
+    title.innerText = "player 1 win!🥇";
+  } else if (resultNum1 < resultNum2) {
+    title.innerText = "player 2 win!🥇";
+  } else {
+    title.innerText = "Draw!";
+  }
+}
+
+// ` `은 문자열 들어가는곳에 다 사용 가능
+function changeDice(res, diceNum) {
+  res.src = `/Dicee Challenge/images/dice${diceNum}.png`;
+}
+function setDice() {
+  return Math.floor(Math.random() * 6) + 1;
+}
